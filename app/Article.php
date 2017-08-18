7a6d40e0b30e9ee040be1fb526e0219247cd68b0
Mailen Havela <mailen.havela@gmail.com>
2017-08-18 15:45:08 -0300

    Many To many relations (with tags) Unable to save a tag (tinker)

--- app/Article.php
+++ app/Article.php
@@ -55,4 +55,15 @@ class Article extends Model
 	{
 		return $this->belongsTo('App\User');
 	}
+	
+	/**
+	 * Get the tag associated with the given article.
+	 * 
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+	 */
+	
+	public function tags()
+	{
+		return $this->belongsToMany('App\Tag');
+	}
 }
